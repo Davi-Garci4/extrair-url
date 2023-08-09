8df8d2551a6c62f4dde9e10b262ebf8f90f36dea
Davi-Garci4 <davi.garcia@aluno.faculdadeimpacta.com.br>
2023-08-09 12:25:53 -0300

    Adicionei o fatiamento da url

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-url = "http://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidade=100"  #Nessa url temos o nome do site/pagina acessada? e os parâmetros.
+url = "http://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidade=100"  #Nessa url temos o nome do site/pagina acessada ? e os parâmetros.
 print(url)
+
+indice_interrogacao = url.find("?")
+
+base_url = url[:indice_interrogacao]
+print(base_url)
+parametro_url = url[indice_interrogacao+1:]
+print(parametro_url)
